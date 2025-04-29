7a3e70d0bc334a8fb305db2a4c1a1c3522371e0d
ikunovic <kunovic.ivan@gmail.com>
2025-04-29 12:17:08 +0200

    Set up GitHub Pages

---
+++ src/wifi_credentials.h
@@ -0,0 +1,8 @@
+#ifndef WIFI_CREDENTIALS_H
+#define WIFI_CREDENTIALS_H
+
+// Replace with your actual WiFi credentials
+const char* ssid = "Serenity";
+const char* password = "12345678";
+
+#endif // WIFI_CREDENTIALS_H 
